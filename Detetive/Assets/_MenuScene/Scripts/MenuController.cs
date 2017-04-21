3769b6f50528fdf372746846012b71e3ced4fc5d
Laza <rafael.securato@gmail.com>
2017-04-21 18:38:27 -0300

    -Adding all qrcode -Begin add sheriff logic

---
+++ Detetive/Assets/_MenuScene/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+
+    /// <summary>
+    /// 0 is Detective
+    /// 1 is Sheriff
+    /// </summary>
+    /// <param name="type"></param>
+    public void SelectGameType(int type)
+    {
+        if (type == 0)
+            SceneManager.LoadScene("SheriffScene");
+        else
+            SceneManager.LoadScene("SheriffScene");
+    }
+}
---
+++ Detetive/Assets/_Shared/Scripts/Enums.cs
@@ -0,0 +1,54 @@
+﻿public class Enums
+{
+    /// <summary>
+    /// Possibles characters to choose
+    /// Value is Qrcode ID
+    /// </summary>
+    public enum Characters
+    {
+        Advogado_Sr_Marinho = 01,
+        Chef_de_Cozinha_Tony_Gourmet = 02,
+        Coveiro_Sergio_Soturno = 03,
+        Dancarina_Srta_Rosa = 04,
+        Florista_Dona_Branca = 05,
+        Medica_Dona_Violeta = 06,
+        Mordomo_James = 07,
+        Sargento_Bicode = 08,
+    }
+
+    /// <summary>
+    /// Possibles Weapon on crime scene
+    /// Value is Qrcode ID
+    /// </summary>
+    public enum Weapons
+    {
+        None,
+        Arma_Quimica = 09,
+        Espingarda = 10,
+        Faca = 11,
+        Pa = 12,
+        Pe_de_Cabra = 13,
+        Soco_Ingles = 14,
+        Tesoura = 15,
+        Veneno = 16
+    }
+
+    /// <summary>
+    /// Possibles Places 
+    /// Value is Qrcode ID
+    /// </summary>
+    public enum Places
+    {
+        Banco = 17,
+        Boate = 18,
+        Cemiterio = 19,
+        Estacao_de_Trem = 20,
+        Floricultura = 21,
+        Hospital = 22,
+        Hotel = 23,
+        Mansao = 24,
+        Praca_Central = 25,
+        Prefeitura = 26,
+        Restaurante = 27
+    }
+}

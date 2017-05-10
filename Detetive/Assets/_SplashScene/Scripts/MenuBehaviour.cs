234a19f0f50cc6fd91ebf60cc1138c0de28c9f3a
Laza <rafael.securato@gmail.com>
2017-05-10 18:57:20 -0300

    Removing <<MenuScene>> from project

--- Detetive/Assets/_SplashScene/Scripts/MenuBehaviour.cs
+++ Detetive/Assets/_SplashScene/Scripts/MenuBehaviour.cs
@@ -1,11 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuBehaviour : MonoBehaviour {
-    
+
+    [SerializeField]
+    private GameObject m_confirmContinue;
+
 	public void OnChooseClick()
     {
+        if (Manager.Instance.isOnGame)
+        {
+            m_confirmContinue.SetActive(true);
+        }
+        else
+        {
+            FindObjectOfType<SplashController>().OpenSelection();
+        }
+    }
+    
+    public void OnConfirmContinueClick()
+    {
+        SceneManager.LoadScene("DetectiveScene");
+    }
+
+    public void OnNewGameClick()
+    {
+        Manager.Instance.MyGameInformation = null;
+        m_confirmContinue.SetActive(false);
         FindObjectOfType<SplashController>().OpenSelection();
     }
 

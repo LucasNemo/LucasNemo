88d06a642c080b9c977c6821ac873ca1beab3aa6
Glauco Belini <Glauco Belini>
2017-05-11 01:49:11 -0300

    Notes done!

---
+++ Detetive/Assets/Notes/Sprites/Notes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public Animator animator; 
+
+    public void NotesClicked()
+    {
+        animator.SetTrigger("ChangeState");
+    }
+}

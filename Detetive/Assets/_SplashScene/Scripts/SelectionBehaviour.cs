8d4f44dc400e39138cc0c8a7fd6e15410b228c46
Laza <rafael.securato@gmail.com>
2017-05-14 11:50:45 -0300

    Change selection (diagonal selection now)

--- Detetive/Assets/_SplashScene/Scripts/SelectionBehaviour.cs
+++ Detetive/Assets/_SplashScene/Scripts/SelectionBehaviour.cs
@@ -5,46 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class SelectionBehaviour : MonoBehaviour
 {
-    [SerializeField]
-    private List<SpriteSelectionBehaviour> m_selectionsButtons;
-
-    private void Start()
-    {
-        SelectItem(0);
-    }
-
     public void OnBackClick()
     {
         FindObjectOfType<SplashController>().OnBackFromSelection();
     }
-
-    public void SelectItem(int index)
-    {
-        m_selectionsButtons.ForEach(x => x.DeselectSprite());
-        m_selectionsButtons[index].SelectSprite();
-    }
-
+    
     public void OpenSettings()
     {
         FindObjectOfType<SplashController>().OpenSettings();
     }
 
-    public void OnPlayClick()
+    public void OnSheriffSelect()
     {
-        int selected = m_selectionsButtons.FindIndex(x => x.IsSelected);
-
-        switch (selected)
-        {
-            case -1:
-            break;
-            case 0:
-                SceneManager.LoadScene("SheriffScene");
-                break;
-            case 1:
-                SceneManager.LoadScene("DetectiveScene");
-                break;
-        }
-
+        SceneManager.LoadScene("SheriffScene");
     }
 
+    public void OnDetectiveSelect()
+    {
+        SceneManager.LoadScene("DetectiveScene");
+    }
+    
 }

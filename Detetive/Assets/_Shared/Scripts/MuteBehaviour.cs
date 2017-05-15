b6b2c2bc32ae3d6835f2f013f09979139ff346fb
Laza <rafael.securato@gmail.com>
2017-05-15 19:31:02 -0300

    Adding a mute canvas (i dont know where i put it so...)

---
+++ Detetive/Assets/_Shared/Scripts/MuteBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteBehaviour : MonoBehaviour {
+
+
+    public void OnMuteClick()
+    {
+        if (AudioController.Instance.IsMusicMuted)
+            AudioController.Instance.UnMuteMusic();
+        else
+            AudioController.Instance.MuteMusic();
+    }
+
+}

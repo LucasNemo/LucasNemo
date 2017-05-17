881f92ccf5fc7bc95a45afde3f7dfe3771e95ef6
Glauco Belini <Glauco Belini>
2017-05-17 11:09:16 -0300

    Fixed modal callbacks

---
+++ Detetive/Assets/_Shared/Scripts/CallTweenStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallTweenStart : MonoBehaviour {
+
+    public EasyTween uiTween;
+
+    public void Start()
+    {
+        uiTween.OpenCloseObjectAnimation();
+    }
+}
